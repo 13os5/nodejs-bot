352635e7cfd104c2811553a4f686ed8a82ae9939
13os5 <b.warakornt@gmail.com>
2018-05-08 16:18:32 +0700

    First

---
+++ app.js
@@ -0,0 +1,37 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var botbuilder_azure = require("botbuilder-azure");
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+ 
+var useEmulator = (process.env.NODE_ENV == 'development');
+var connector = useEmulator ? new builder.ChatConnector() : new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector);
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+bot.set('storage', inMemoryStorage);
+
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+// Main dialog with LUIS
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+.matches('Greeting', (session) => {
+    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+})
+.match
